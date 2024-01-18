d8e1bc3fc59ed720b5f44cea0ba1e44bfff0d971
benlaguroun <benlaguroun@gmail.com>
2024-01-18 04:19:05 -0800

    task advance

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Create a Python function to count words in hot posts' titles on a Reddit subreddit """
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """ Outputs the occurrences of specified words in the titles of hot posts from a given subreddit
+    Parameters:
+        subreddit (str):  The target subreddit
+        word_list (list): List of words to search for in post titles
+        instances (obj): Key-value pairs of words and their counts
+        after (str): Parameter for navigating to the next page of API results
+        count (int): The parameter of results matched thus far
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
