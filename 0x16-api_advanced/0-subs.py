20b2f25749d56dcffe5b34cd863f97a4d8e4621b
benlaguroun <benlaguroun@gmail.com>
2024-01-18 04:01:32 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-""" Function to retrieve the number of subscribers for a given Reddit subreddit """
+
 import requests
 
 def number_of_subscribers(subreddit):
-    """Retrieve the total number of subscribers for a specified subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """Returns the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Check for HTTP errors
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    except requests.exceptions.HTTPError as e:
+        if response.status_code == 404:  # Invalid subreddit
+            return 0
+        else:
+            raise e  # Raise other HTTP errors
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
 
